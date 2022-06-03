17fc4e1d64531a8fe426e6b66b03f3a3942dce6e
crmuelle <christopher.mueller@be-storaged.com>
2022-06-03 20:29:26 +0200

    added logging to version checker

--- mapping/bulkupdate/version_checker.py
+++ mapping/bulkupdate/version_checker.py
@@ -1,7 +1,12 @@
+import logging
 import json
 import requests
+from mapping.core.base_logger import BaseLogger
+
+conf = BaseLogger(tags="['BulkUpdate']['VersionChecker']")
+logger = logging.getLogger(__name__)
 
 url = 'https://raw.githubusercontent.com/SpielerNogard/mapping/bulkupdate/versions.json'
 resp = requests.get(url)
 data = json.loads(resp.text)
-print(data)
+logger.info(data)
